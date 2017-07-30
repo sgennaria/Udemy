b2ff3dd1b9f4f3f831cd8d89ec5eb043e649b4dd
sgennaria <sgennaria@gmail.com>
2017-07-29 23:34:46 -0400

    1

--- Data Science and Machine Learning Bootcamp with R/Linear Regression.r
+++ Data Science and Machine Learning Bootcamp with R/Linear Regression.r
@@ -2,6 +2,7 @@ df <- read.csv('C:\\Users\\sgenn_000\\Documents\\GitHub\\Udemy\\Data Science and
 head(df)
 summary(df)
 
+# make sure there are no NA/NULL values before analyzing
 any(is.na(df))
 any(is.null(df))
 
@@ -19,8 +20,8 @@ cor.data <- cor(df[,num.cols])
 
 cor.data
 
-install.packages('corrgram')
-install.packages('corrplot')
+#install.packages('corrgram')
+#install.packages('corrplot')
 
 library(corrgram)
 library(corrplot)
@@ -31,22 +32,47 @@ ggplot(df, aes(x=G3)) + geom_histogram(bins=20, alpha=0.5, fill='blue') + theme_
 
 library(caTools)
 
+# set the seed to have the same results as the Udemy course
 set.seed(101)
-sample <- sample.split(df$age, SplitRatio = 0.70)
+# split the desired column into a training set and a test set
+# the radio 0.7 means 70% will be training data
+# and 30% will be test data
+#sample <- sample.split(df$age, SplitRatio = 0.70)
+sample <- sample.split(df$G3, SplitRatio = 0.70)
 
+# create individual data sets for the training and test data
 train = subset(df, sample==TRUE)
 test = subset(df, sample==FALSE)
 
+# create a linear regression (lm) model
+# basic structure: 
+# model <- lm(y ~ x1 + x2, data)
+#                  ^    ^
+#      individual features to predict upon
+# 
+# model <- lm(y ~ . , data)
+#                 ^
+#   predict upon ALL variables/features in the data set  
+
+# train and build model using ALL features of the training data set
 model <- lm(G3 ~ .,train)
+
+# view the results:
+# Estimated Coefficient is the value of slope calculated by the regression.  Without normalized data, we cannot effectively compare these between variables.
+# Coefficient Standard Error is a measure of the variability of each estimated coefficient (lower is usually better, at least an order of magnitude less than the coefficient estimate)
+# T-Value is a measure of whether or not the coefficient for the variable is meaningful for the model
+# Probability (P) is the probability that the variable is NOT relevant.  Smaller is better.  *** asterisks indicate significance.
+# R-squared value is a metric for evaluating the goodness of fit for the model.  Higher is better (max 1).  It corresponds with the amount of variability in your predictions which are explained by the model.
 summary(model)
 
+# plot a histogram of the residuals
+# we would like the residuals to be normally distributed, in order to indicate that the mean of the diff between the predictions and actual values is close to 0 (indicating roughly equal distance between actual data above and below our predictions)
+# this plot shows suspicious negative residuals which indicate predicted negative test scores, which are actually impossible since the lowest test score is 0
 res <- residuals(model)
-
 res <- as.data.frame(res)
-
 head(res)
-
 ggplot(res, aes(res)) + geom_histogram(fill='blue', alpha=0.5)
+
 
 plot(model)
 
@@ -74,3 +100,6 @@ SSE = sum((results$pred - results$real)^2)
 SST = sum((mean(df$G3) - results$real)^2)
 R2 = 1 - SSE/SST
 R2 # R-Squared Value of model predictions
+
+
+
